a920ae804b9fc7fa6e3369fdaa256b1ab805424c
LstChanceMadrid <michael.madrid@live.com>
2018-09-14 15:59:38 -0500

    personalized code formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,8 @@
 const activateTab = (selectedTab, selectedTabBody, inactiveTabOne, inactiveTabOneBody, inactiveTabTwo, inactiveTabTwoBody) => {
     document.getElementById(selectedTab).style.background = "rgba(150, 150, 150, 0.5)";
     document.getElementById(selectedTabBody).style.display = "block";
-    document.getElementById(inactiveTabOne).style.background = "lightcoral";
+    document.getElementById(inactiveTabOne).style.background = "rgb(236, 17, 17)";
     document.getElementById(inactiveTabOneBody).style.display = "none";
-    document.getElementById(inactiveTabTwo).style.background = "lightcoral";
+    document.getElementById(inactiveTabTwo).style.background = "rgb(236, 17, 17)";
     document.getElementById(inactiveTabTwoBody).style.display = "none";
-}
-
-const turnRed = (id) => {
-    document.getElementById(id).style.color = "red";
-}
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
 }
